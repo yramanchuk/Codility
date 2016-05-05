cd71de4e608d8c7d900bcdb7ec180ec7a1fd28c2
YuryR <yramanchuk@users.noreply.github.com>
2016-05-05 13:51:52 +0300

    add counting array slices solutions

--- Codility/Tasks/Lesson_9-MaxSliceProblem/MaxSliceSum.m
+++ Codility/Tasks/Lesson_9-MaxSliceProblem/MaxSliceSum.m
@@ -63,4 +63,50 @@
 //    return maxProfit;
 //}
 
+
+
+
+//Write a function getProductsOfAllIntsExceptAtIndex() that takes an array of integers and returns an array of the products.
+//
+//For example, given:
+//
+//[1, 7, 3, 4]
+//
+//your function would return:
+//
+//[84, 12, 28, 21]
+//
+//by calculating:
+//
+//[7*3*4, 1*3*4, 1*7*4, 1*7*3]
+//
+//Do not use division in your solution.
+
+//public int[] getProductsOfAllIntsExceptAtIndex(int[] intArray) {
+//    
+//    // we make an array with the length of the input array to
+//    // hold our products
+//    int[] productsOfAllIntsExceptAtIndex = new int[intArray.length];
+//    
+//    // for each integer, we find the product of all the integers
+//    // before it, storing the total product so far each time
+//    int productSoFar = 1;
+//    for (int i = 0; i < intArray.length; i++) {
+//        productsOfAllIntsExceptAtIndex[i] = productSoFar;
+//        productSoFar *= intArray[i];
+//    }
+//    
+//    // for each integer, we find the product of all the integers
+//    // after it. since each index in products already has the
+//    // product of all the integers before it, now we're storing
+//    // the total product of all other integers
+//    productSoFar = 1;
+//    for (int i = intArray.length - 1; i >= 0; i--) {
+//        productsOfAllIntsExceptAtIndex[i] *= productSoFar;
+//        productSoFar *= intArray[i];
+//    }
+//    
+//    return productsOfAllIntsExceptAtIndex;
+//}
+
 @end
