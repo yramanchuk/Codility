2e630824de79479b5ebbde9b0077db6fedd3ab1d
YuryR <yramanchuk@users.noreply.github.com>
2016-05-26 23:49:01 +0300

    add tree comparison

--- Codility/CTCI/Algorithms.m
+++ Codility/CTCI/Algorithms.m
@@ -833,6 +833,53 @@ static id _instance = nil;
     }
 }
 
+/**
+ * Definition for binary tree
+ */
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+typedef struct TreeNode treenode;
+
+treenode* treenode_new(int val) {
+    treenode* node = (treenode *) malloc(sizeof(treenode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+/**
+ * @input A : Root pointer of the tree
+ * @input B : Root pointer of the tree
+ *
+ * @Output Integer
+ */
+int isSameTree(treenode* A, treenode* B) {
+    
+    if (!A && !B) {
+        return 1;
+    } else if (!A || !B) {
+        return 0;
+    }
+    
+    if (A->val != B->val) {
+        return 0;
+    }
+    if (isSameTree(A->left, B->left) == 0) {
+        return 0;
+    }
+    
+    if (isSameTree(A->right, B->right) == 0) {
+        return 0;
+    }
+    
+    return 1;
+}
+
 
 #pragma mark tree mirroring
 + (EKTreeNode *)mirror:(EKTreeNode *)input {
